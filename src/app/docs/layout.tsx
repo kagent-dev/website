0f427436e699cf399d545e19133648f67fde6b7c
Peter Jausovec <peter.jausovec@solo.io>
2025-03-27 15:14:52 +0100

    add modeloptions page
    Signed-off-by: Peter Jausovec <peter.jausovec@solo.io>

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -28,6 +28,7 @@ const navigation: NavItem[] = [
       { title: "Quick Start", href: "/docs/getting-started/quickstart" },
       { title: "Your First Agent", href: "/docs/getting-started/first-agent" },
       { title: "Writing System Prompts", href: "/docs/getting-started/system-prompts" },
+      { title: "Configuring LLM Providers", href: "/docs/getting-started/configuring-providers" },
 
     ],
   },
